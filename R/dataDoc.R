9e92f326b7bb2e00681afca38231ca8887c4945c
dtn2 <dtn2@hood.edu>
2025-02-13 20:01:20 -0500

    create test

---
+++ R/dataDoc.R
@@ -0,0 +1,9 @@
+#' MSfrags
+#'
+#' some fragments with protein modifications that we might expect
+#' to find from the CCR5 gen in a mass spec experiment 
+#'
+#' @format A character vector of length 4 with peptide sequence
+#'
+#' @source data-raw/data.R
+"MSfrags"
